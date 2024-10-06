db386ae217fff1a50a3272a995c7080546ddc188
Stacy Cashmore <33732891+StacyCash@users.noreply.github.com>
2024-10-06 21:59:10 +0200

    Split the Client and Api auth projects, deploy as 2 NuGet packages

--- StacyClouds.SwaAuth.Api/StaticWebAppApiAuthentication.cs
+++ StacyClouds.SwaAuth.Api/StaticWebAppApiAuthentication.cs
@@ -1,24 +1,26 @@
 ﻿using System.Text;
 using System.Text.Json;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.Functions.Worker.Http;
 using StacyClouds.SwaAuth.Models;
 
 namespace StacyClouds.SwaAuth.Api;
 
 public static class StaticWebAppApiAuthentication
 {
-    public static ClientPrincipal ParseHttpHeaderForClientPrinciple(IHeaderDictionary headers)
-    {
-        if (!headers.TryGetValue("x-ms-client-principal", out var header))
-        {
-            return new ClientPrincipal();
-        }
+	private static readonly JsonSerializerOptions JsonSerializerOptions = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
 
-        var data = header[0];
-        var decoded = Convert.FromBase64String(data);
-        var json = Encoding.UTF8.GetString(decoded);
-        var principal = JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+	public static ClientPrincipal ParseHttpHeaderForClientPrinciple(HttpHeadersCollection headers)
+	{
+		if (!headers.TryGetValues("x-ms-client-principal", out var header))
+		{
+			return new ClientPrincipal();
+		}
 
-        return principal ?? new ClientPrincipal();
-    }
+		var data = header.FirstOrDefault(string.Empty);
+		var decoded = Convert.FromBase64String(data);
+		var json = Encoding.UTF8.GetString(decoded);
+		var principal = JsonSerializer.Deserialize<ClientPrincipal>(json, JsonSerializerOptions);
+
+		return principal ?? new ClientPrincipal();
+	}
 }
